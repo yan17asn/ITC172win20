08adc318ae60c1b010f566d0689a307a65b7d830
yan17asn <248764378@qq.com>
2020-02-05 22:23:36 -0800

    Add files via upload

--- PythonClub/Club/urls.py
+++ PythonClub/Club/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-
+    path('getResource/', views.getResource, name="resource"),
+    path('getMeeting/', views.getMeeting, name="meeting"),
+    path('getmeetingDetail/', views.meetingDetails, name="meetingdetail")
 ]
--- PythonClub/Club/views.py
+++ PythonClub/Club/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request,'Club/index.html')
+def getResource(request):
+    resource_list = Resource.objects.all()
+    context={'resource_list':resource_list}
+    return render(request, 'Club/resource.html', context=context)
+
+def getMeeting(request):
+    meeting_list = Meeting.objects.all()
+    context={'meeting_list':meeting_list}
+    return render(request, 'Club/meeting.html', context=context)
+
+def meetingDetails(request):
+    meetDt=get_object_or_404(Meeting, pk=meetingtitle)
+    date=Meeting.meetingdate
+    location=Meeting.location
+    context={
+        'meetDT':meetDt,
+        'date':date,
+        'location':location,
+    }
+    return render(request, 'Club/meetingDetail.html', context=context)
